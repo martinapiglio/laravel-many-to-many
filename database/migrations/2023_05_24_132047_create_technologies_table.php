67ba4d67e5b94c683ac3b6148cc008c5bdab8f9b
martinapiglio <martina.piglio@gmail.com>
2023-05-24 15:24:33 +0200

    feat: Technology migration fields added

--- database/migrations/2023_05_24_132047_create_technologies_table.php
+++ database/migrations/2023_05_24_132047_create_technologies_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name',100)->unique();
+            $table->string('color',7)->nullable();
+            $table->string('slug',100);
+            
             $table->timestamps();
         });
     }
