57514deb4c43cb853421a83e57d0db88ee556e1a
martinapiglio <martina.piglio@gmail.com>
2023-05-24 15:30:23 +0200

    feat: db seed via Technology seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class TechnologySeeder extends Seeder
@@ -12,8 +15,19 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $technologies = ['VS Code', 'Figma', 'HTML', 'CSS', 'Sass', 'Scss', 'JS', 'Vue', 'Bootstrap', 'PHP', 'MySQL', 'Laravel'];
+
+        foreach($technologies as $technology){
+
+            $newTechnology = new Technology();
+
+            $newTechnology->name = $technology;
+            $newTechnology->color = $faker->hexColor();
+            $newTechnology->slug = Str::slug($newTechnology->name, '-');
+
+            $newTechnology->save();
+        }
     }
 }
